ea68ca89ea5f0b219c5df9e9094f1f96e37fa872
Rume Dolor <doghenerume@gmail.com>
2022-06-30 20:15:48 -0700

    gcc 6-size.c -m32 -o size32 2> /tmp/32 edit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-	printf("size of a long int: %lu byte(s)\n", sizeof(li));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
