abdf752ac1dc4ef4ba5d6c53fe8a02f0c7b9c2df
Rume Dolor <doghenerume@gmail.com>
2022-07-01 19:37:15 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(i > j || i == j))
+			{
+				putchar(i);
+				putchar(j);
+
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			j++;
+		}
+
+		j = '0';
+		i++;
+	}
+
+	return (0);
+}
