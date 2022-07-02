a32b6b5e1c73d2c19069e28da73c172d289cc490
Rume Dolor <doghenerume@gmail.com>
2022-07-01 19:56:03 -0700

    101 combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a = '0';
+	int b = '0';
+	int c = '0';
+
+	while (a <= '7')
+	{
+		while (b <= '8')
+		{
+			while (c <= '9')
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (!(a == '7' && b == '8' && c == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+				c++;
+			}
+
+			c = '0';
+			b++;
+		}
+
+		b = '0';
+		a++;
+	}
+}
