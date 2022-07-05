84be946f7363870f88be69a6088a1bbddb1a0e43
Rume Dolor <doghenerume@gmail.com>
2022-07-05 05:45:53 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - a program that prints _putchar
+ * followed by a new line
+ * 
+ * *by rume_dolor
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar\0";
+
+	_putchar(str);
+
+	return (0);
+}
