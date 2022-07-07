82bef604fb83aece1bd8b49bb2b3af24750ad671
Rume Dolor <doghenerume@gmail.com>
2022-07-07 07:18:22 -0700

    6-lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line using _ character
+ * followed by a new line
+ *
+ * @n: parameter indicating number of times _ line should
+ * be printed
+ *
+ * Return: Nothing
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
