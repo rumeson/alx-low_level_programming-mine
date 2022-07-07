47394d045c41fc75fc189fa62f20310744e23a91
Rume Dolor <doghenerume@gmail.com>
2022-07-06 19:28:08 -0700

    103 edit

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - sum even fibonacci numbers under 4 million
  *
- * Return: Always 0
+ * Return: Nothing.
  */
 
 int main(void)
 {
-	unsigned int a = 1, b = 2, c;
+	unsigned long count, i, j, k, sums;
 
-	printf("%u, %u, ", a, b);
+	i = sums = 0;
+	j = 1;
 
-	while (c <= 4000000)
+	for (count = 0; count < 50; count++)
 	{
-		c = a + b;
+		k = i + j;
+		i = j;
+		j = k;
 
-		if (c + b > 4000000)
-			printf("%u", c);
-
-		else if (c % 2 == 0)
-			printf("%u, ", c);
-
-		a = b;
-		b = c;
+		if (k % 2 == 0 && k < 4000000)
+		{
+			sums += k;
+		}
 	}
 
-	printf("\n");
+	printf("%lu\n", sums);
 
 	return (0);
 }
