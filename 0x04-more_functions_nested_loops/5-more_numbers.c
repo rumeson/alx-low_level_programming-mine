cf90cede842cdb98986967670800b026bd7f3a1f
Rume Dolor <doghenerume@gmail.com>
2022-07-07 07:07:59 -0700

    5-more_numbers_edit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,12 @@ void more_numbers(void)
 	for (j = 0; j <= 9; j++)
 	{
 		for (i = 0; i <= 14; i++)
+		{
 			if (i > 9)
-			{
 				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else
-				_putchar(i + '0');
+
+		_putchar((i % 10) + '0');
+		}
 
 		_putchar('\n');
 	}
