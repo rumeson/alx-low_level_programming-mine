d6ef35f7bf7c700f0a41e14299c991351f9bdc38
Rume Dolor <doghenerume@gmail.com>
2022-07-07 07:03:01 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints number 0 to 14 ten times
+ * followed by a new line
+ *
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+				_putchar(i + '0');
+
+		_putchar('\n');
+	}
+}
