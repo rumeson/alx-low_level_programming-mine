7059b585f471aebf6c3a90b82ee0c4fa6778bd73
Rume Dolor <doghenerume@gmail.com>
2022-07-06 17:04:33 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,50 +11,53 @@ void print_times_table(int n)
 {
 	int a, b;
 
-	for (b = 0; b <= n; b++)
+	if (n < 100)
 	{
-		for (a = 0; a <= n; a++)
+		for (b = 0; b <= n; b++)
 		{
-			if ((a * b) > 9 && (a * b) <= 99)
+			for (a = 0; a <= n; a++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(((a * b) / 10) + '0');
-				_putchar(((a * b) % 10) + '0');
-			}
-			else if ((a * b) > 99 && (a * b) <= 999)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(((a * b) / 100) + '0');
-				_putchar((((a * b) % 100) / 10) + '0');
-				_putchar((((a * b) % 100) % 10) + '0');
-			}
-			else if ((a * b) >= 100)
-			{
-/**				_putchar(',');
-				_putchar(' ');
-				_putchar(((a * b) / 1000) + '0');
-				_putchar((((a * b) % 1000) / 100) + '0');
-				_putchar(((((a * b) % 1000) % 100) / 10) + '0');
-				_putchar(((((a * b) % 1000) % 100) % 10) + '0');*/
-			}
-			else if (a * b <= 9)
-			{
-				if (a == 0)
-					_putchar((a * b) + '0');
-				else
+				if ((a * b) > 9 && (a * b) <= 99)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(((a * b) / 10) + '0');
+					_putchar(((a * b) % 10) + '0');
+				}
+				else if ((a * b) > 99 && (a * b) <= 999)
+				{
+					_putchar(',');
 					_putchar(' ');
-					_putchar((a * b) + '0');
+					_putchar(((a * b) / 100) + '0');
+					_putchar((((a * b) % 100) / 10) + '0');
+					_putchar((((a * b) % 100) % 10) + '0');
+				}
+				else if ((a * b) >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(((a * b) / 1000) + '0');
+					_putchar((((a * b) % 1000) / 100) + '0');
+					_putchar(((((a * b) % 1000) % 100) / 10) + '0');
+					_putchar(((((a * b) % 1000) % 100) % 10) + '0');
+				}
+				else if (a * b <= 9)
+				{
+					if (a == 0)
+						_putchar((a * b) + '0');
+					else
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((a * b) + '0');
+					}
 				}
 			}
+	
+			_putchar('\n');
 		}
-
-		_putchar('\n');
 	}
 }
