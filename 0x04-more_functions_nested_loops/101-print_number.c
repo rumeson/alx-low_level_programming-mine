34d3905fa32cf245a8b273c67a1e4f597a37b738
Rume Dolor <doghenerume@gmail.com>
2022-07-07 18:04:44 -0700

    101 print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,44 +1,33 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
  *
- * @n: parameter indicating number to print
+ *  * print_number - Prints an integer.
  *
- * Return: Nothing
- */
+ *   * @n: The integer to be printed.
+ *
+ *    */
 
 void print_number(int n)
+
 {
-	if (n >= 0 && n <= 9)
-		_putchar(n + '0');
-	else if (n >= 10 && n <= 99)
+
+	unsigned int num = n;
+
+	if (n < 0)
+
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n >= 100 && n <= 999)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n % 100) / 10) + '0');
-		_putchar(((n % 100) % 10) + '0');
-	}
-	else if (n >= 1000 && n <= 9999)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n % 1000) / 100) + '0');
-		_putchar((((n % 1000) % 100) / 10) + '0');
-		_putchar((((n % 1000) % 100) % 10) + '0');
-	}
-	else if (n < 0 && n >= -9)
-	{
+
 		_putchar('-');
-		_putchar((-n % 10) + '0');
+
+		num = -num;
+
 	}
-	else if (n < -9 && n >= -99)
-	{
-		_putchar('-');
-		_putchar((-n / 10) + '0');
-		_putchar((-n % 10) + '0');
-	}
+
+	if ((num / 10) > 0)
+
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+
 }
