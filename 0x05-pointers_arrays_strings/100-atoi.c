3cf280fd88e37b6ae6c8888ecc39ae12cb38efa7
Rume Dolor <doghenerume@gmail.com>
2022-07-12 18:09:50 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,16 +11,14 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	do
-	{
+	do {
 		if (*s == '-')
 			sign *= -1;
 		else if	(*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
-	}
-	while
+	} while
 		(*s++);
-	return (num *sign);
+	return (num * sign);
 }
