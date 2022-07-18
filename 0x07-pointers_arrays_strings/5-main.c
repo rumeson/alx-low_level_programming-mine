03f6c5528d1a4294c45889a9cbb3d355aecdefc1
Rume Dolor <doghenerume@gmail.com>
2022-07-18 05:09:26 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -6,13 +6,14 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    char *s = "hello, world";
-    char *f = "world";
-    char *t;
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
 
-    t = _strstr(s, f);
-    printf("%s\n", t);
-    return (0);
-}
+	t = _strstr(s, f);
+	printf("%s\n", t);
+	return (0);
+	}
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,45 @@
+#include "main.h"
+#include <stddef.h>
+/**
+* _strstr - a function that locates a substring
+* @haystack: pointer to string
+* @needle: pointer to substring
+*
+* Return: Nothing
+*/
 
+char *_strstr(char *haystack, char *needle)
+{
+	int len_n = 0, j, i, n;
+	int len_h = 0;
+
+	while (*needle != 0)
+	{
+		len_n++;
+		needle++;
+	}
+	while (*haystack != 0)
+	{
+		len_h++;
+		haystack++;
+	}
+	needle = needle - len_h;
+	haystack = haystack - len_h;
+	for (i = 0; i < len_h; i++)
+	{
+		j = 0;
+		n = i;
+		if (*(haystack + i) == ' ')
+			continue;
+		if (*(needle + j) != *(haystack + i))
+			continue;
+		for ( ; j < len_n; j++)
+		{
+			if (*(needle + j) != *(haystack + n))
+				break;
+		}
+		return (&haystack[i]);
+
+	}
+	return (NULL);
+}
