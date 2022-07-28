025d08d4371a72cff262cd5908b79ad8be2db122
Rume Dolor <doghenerume@gmail.com>
2022-07-27 23:03:19 -0700

    g

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,17 +2,47 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: a pointer to a newly allocated space in memory which
+ * contains the contents of s1, followed by the contents of s2,
+ * and null terminated. NULL on failure
+ */
+
 char *str_concat(char *s1, char *s2)
 {
-	/*int len1, len2;
+	int i, j, len1, len2, len;
+	char *result;
 
 	len1 = len2 = 0;
 
-	while (*s1 != 0)
-		len1++;
-	while (*s2 != 0)
-		len2++;*/
-	printf("first letter: %c, second letter : %c\n", *s1, *s2);
-	/*printf("len1 : %d, len2: %d\n", len1, len2);*/
-	return (NULL);
+	if (s1 != NULL)
+	{
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
+	}
+
+	if (s2 != NULL)
+	{
+		i = 0;
+		while (s2[i++] != '\0')
+			len2++;
+	}
+
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
+
+	return (result);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,24 +1,41 @@
 #include "main.h"
-#include <stddef.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
-* alloc_grid -  a function that returns a
-* pointer to a 2 dimensional array of integers
-* @width: width of array
-* @height: height of array
-*
-* Return: pointer to array
-*/
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: a pointer to a 2 dimensional array of integers.
+ */
 
 int **alloc_grid(int width, int height)
 {
-	int *grid;
+	int **array, i, j;
+	int len = width * height;
 
-	*grid = (malloc (sizeof(width) * width + sizeof(height) * height));
-	return (grid);
-
-	if (width <= 0 || height <= 0)
+	if (len <= 0)
 		return (NULL);
+
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(array[i]);
+			free(array);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+
+	return (array);
 }
