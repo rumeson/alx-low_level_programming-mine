3cd3f55c08f324a4f29a18a3b4c42803e68c1d01
Rume Dolor <doghenerume@gmail.com>
2022-07-28 07:50:01 -0700

    edit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,68 +1,54 @@
 #include "main.h"
-#include "stddef.h"
-#include "stdlib.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+int _strlen(char *s);
 
 /**
-* get_len - get length
-* @s: string
-* @len: length
-*
-* Return: length
-*/
-
-int get_len(char *s, int len)
-{
-	while (s[len] != '\0')
-		len++;
-	return (len);
-}
-
-/**
-* string_nconcat - concatenates two strings
-* @s1: first string
-* @s2: second string
-* @n: n number of s2 string
-*
-* Return: pointer to new string
-*/
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to concat
+ *
+ * Return: pointer to a newly allocated space in memory
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2, len, i, j;
-	char *ptr;
+	char *p;
+	unsigned int len, i, c;
 
-	j = len1 = len2 = len = 0;
-	len1 = get_len(s1, len1);
-	len2 = get_len(s2, len2);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	ptr = malloc(sizeof(*s1) * (len1 + n + 1));
-	if (ptr == NULL)
+	len = (unsigned int)_strlen(s1);
+	p = malloc((len + n + 1) * sizeof(char));
+	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
+	for (i = 0, c = 0; i < (len + n); i++)
 	{
-		ptr[j] = s1[i];
-		j++;
+		if (i < len)
+			p[i] = s1[i];
+		else
+			p[i] = s2[c++];
 	}
-	if (n >= len2)
-	{
-		for (i = 0; i < len2; i++)
-		{
-			ptr[j] = s2[i];
-			j++;
-		}
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			ptr[j] = s2[i];
-			j++;
-		}
-	}
-	ptr[j] = '\0';
-	return (ptr);
+	p[i] = '\0';
+
+	return (p);
 }
+
+/**
+ * _strlen - gets the length of a string
+ * @s: the string
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++);
+	return (i);
+
