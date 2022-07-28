47386b4120538c42a8cbad790a2e5bdd6a97210e
Rume Dolor <doghenerume@gmail.com>
2022-07-28 13:50:45 -0700

    calloc__edit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (!nmemb || !size)
 		return (NULL);
 
-	ptr = malloc((nmemb * size) + 1);
+	ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
 		return (NULL);
