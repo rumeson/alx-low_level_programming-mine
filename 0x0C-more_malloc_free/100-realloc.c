055dab677d56ebc210664ace474ddf3aab613301
Rume Dolor <doghenerume@gmail.com>
2022-07-28 20:06:06 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,7 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	free(ptr);
 
-	ptr = malloc(new_size * sizeof(unsigned int));
+	ptr = malloc(new_size * sizeof(unsigned int) + 1);
 
 	return (ptr);
 }
