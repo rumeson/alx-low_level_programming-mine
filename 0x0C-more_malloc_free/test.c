c8020af68e68647caa8ce057d726ba2dbadee67e
Rume Dolor <doghenerume@gmail.com>
2022-07-31 17:17:46 -0700

    preprocessor

---
+++ 0x0C-more_malloc_free/test.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+
+
+int main(void)
+
+{
+
+    NULL;
+
+    return (EXIT_SUCCESS);
+
+}
---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print file name
+*
+* Return: 0 Always
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
