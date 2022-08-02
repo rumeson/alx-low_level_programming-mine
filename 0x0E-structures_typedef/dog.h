6eccef790d54543a0f45ca29546fb0c8ad8ee77f
Rume Dolor <doghenerume@gmail.com>
2022-08-01 18:05:57 -0700

    dog_edit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,18 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - description for dog struct
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+*/
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* DOG_H */
