1403831474c3d6e050ff6aadd81cfbcf9c76987d
Rume Dolor <doghenerume@gmail.com>
2022-08-11 12:35:04 -0700

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* print_list - prints all the elements of a list_t list
+* @h: pointer to node
+* Return: number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
