df46351093a816436eb1bcf2486396488d435190
Rume Dolor <doghenerume@gmail.com>
2022-08-11 20:52:36 -0700

    add_last_node

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+* add_node_end -  adds a new node at the end of a list_t list.
+* @head: head of list_t list
+* @str: string to be added to list_t list
+* Return: address of the new element, or NULL if it failed
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		list_t *last_node = *head;
+		while (last_node->next)
+			last_node = last_node->next;
+		last_node->next = new_node;
+	}
+
+	return (*head);
+}
