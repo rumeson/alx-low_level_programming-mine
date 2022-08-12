bc03a4abc8062556baf243630ea790e7eaf24fa2
Rume Dolor <doghenerume@gmail.com>
2022-08-11 21:15:22 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_list - function that frees a list_t list.
+* @head: point to list_t list
+* Return: Nothing
+*/
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
