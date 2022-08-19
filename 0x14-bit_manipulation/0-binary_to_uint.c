caf3c16ed2c267a00ed1ec4d7918e5db07285ba8
Rume Dolor <doghenerume@gmail.com>
2022-08-18 20:07:32 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0;
-	int j;
+	unsigned int i, res = 0;
 
 	if (!b)
 		return (0);
 
-	while (*b != '\n')
+
+	for (i = 0; b[i]; i++)
 	{
-		j = atoi(*b);
-		i++;
-		b++;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		res = 2 * res + (b[i] - '0');
 	}
-	printf("%d    ,    %d\n", j, i);
-	return (0);
+	return (res);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
